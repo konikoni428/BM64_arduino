31fa8aee4716010de829ac90c270f92226e62c99
Masahiro Kinishi <konikoni428@gmail.com>
2021-06-09 10:43:31 +0900

    change debug macro name

--- src/BM64_Debug.h
+++ src/BM64_Debug.h
@@ -1,9 +1,9 @@
 #ifndef BM64_Debug_h_
 #define BM64_Debug_h_
 
-// #define DEBUG // Switch debug output on and off by commentout 
+// #define BM64_DEBUG // Switch debug output on and off by commentout 
 
-#ifdef DEBUG
+#ifdef BM64_DEBUG
 #include <SoftwareSerial.h>
 #define USE_DEBUG       extern SoftwareSerial _swSerial;
 #define DPRINT(...)     _swSerial.print(__VA_ARGS__)
